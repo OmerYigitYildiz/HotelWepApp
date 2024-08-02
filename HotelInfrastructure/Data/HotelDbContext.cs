d50a38c2f4530fad33bae976e735285c96567d29
OmerYigitYildiz <omeryigityildiz0@gmail.com>
2024-08-02 12:26:25 +0300

    HotelDbContext

--- HotelInfrastructure/Data/HotelDbContext.cs
+++ HotelInfrastructure/Data/HotelDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using HotelDomain.Entities;
+using HotelDomain.Entities;
+using HotelDomain.Entities.BaseEntity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,7 +17,28 @@ namespace HotelInfrastructure.Data
 
         }
 
+        //public DbSet<Ids> Ids { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Room> Room { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Author>()
+                .HasMany(a => a.Books)
+                .WithOne(b => b.Author)
+                .HasForeignKey(b => b.AuthorId);
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Book>()
+        //        .HasOne(t => t.AuthorName)
+        //        .WithMany(p => p.Books)
+        //        .HasForeignKey(t => t.AuthorId);
+        //}
     }
 }
