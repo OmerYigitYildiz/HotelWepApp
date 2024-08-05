f36938ea15080f98104cf1cb706e9036ca1184f4
OmerYigitYildiz <omeryigityildiz0@gmail.com>
2024-08-05 10:57:18 +0300

    AddLogs

---
+++ HotelApplication/Logging/CheckDbConnection.cs
@@ -0,0 +1,31 @@
+﻿using Serilog;
+using System;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace HotelApplication.Logging
+{
+    public class CheckDbConnection
+    {
+        private readonly string _connectionString;
+
+        public CheckDbConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task CheckConnectionAsync()
+        {
+            try
+            {
+                await using var connection = new NpgsqlConnection(_connectionString);
+                await connection.OpenAsync();
+                Log.Information("PostgreSQL bağlantısı başarılı.");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "PostgreSQL bağlantısı kurulamadı.");
+            }
+        }
+    }
+}
